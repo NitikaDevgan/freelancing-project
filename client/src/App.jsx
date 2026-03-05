781212bb6dce075325c6d89a33ac70ef8d67b264
Nitika Devgan <nitikadevganisnd@gmail.com>
2026-03-05 14:02:51 +0530

    frontend code change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from 'react'
-import './App.css'
 import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      return <Home />;
-    </>
-  )
+   <>
+   <Home />
+   </>
+  );
 }
 
-export default App
+export default App;
