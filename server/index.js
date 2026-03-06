ab32c9550c72430e01d1536f8f5dfc334dd01bb1
Nitika Devgan <nitikadevganisnd@gmail.com>
2026-03-06 22:40:52 +0530

    change in the backend service

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import contactRoutes from "./routes/contactRoutes.js";
 
 dotenv.config();
 
@@ -10,15 +9,15 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/contact", contactRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("API Running...");
+  res.send("API Running");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
